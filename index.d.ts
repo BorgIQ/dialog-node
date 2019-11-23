2ed8605592f85f6b8dd5d32c06c68af515873bac
Baskar Puvanathasan <baskarp@users.noreply.github.com>
2019-11-22 23:17:54 -0500

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,7 @@
+
 export function entry(str: string, title: string, timeout: number, hidden: boolean, callback: Function): void;
+export function error(str: string, title: string, timeout: number, callback: Function): void;
+export function info(str: string, title: string, timeout: number, callback: Function): void;
+export function warn(str: string, title: string, timeout: number, callback: Function): void;
+export function question(str: string, title: string, timeout: number, callback: Function): void;
+export function fileselect(str: string, title: string, timeout: number, folder: boolean, callback: Function): void;
